247a9531c6be1c1c2b47fd2d64c1db4072b1d7a4
Yuliya <juliya_s_07@mail.ru>
2021-09-19 15:30:13 +0300

    Add obj personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-console.log(numberOfFilms);
+// console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
